13f9292eb7dd16191d1f67ed81c013fb28bea478
André Ruffert <andre@andreruffert.com>
2020-02-01 02:42:03 +0100

    feat: image loading on slow connection needs action
    Only load data needed. Checks for the connection effective type. If the connection is slow (slow-2g|2g|3g) loading the actual image needs a click action.

--- progressive-image-element.js
+++ progressive-image-element.js
@@ -6,24 +6,7 @@ class ProgressiveImageElement extends HTMLElement {
     this._observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          this._image.onload = e => e.target.classList.add('loaded');
-          this._image.src = entry.target.src;
-          this._image.srcset = entry.target.srcset;
-          this._image.style.position = 'absolute';
-          this._image.style.top = '0';
-          this._image.style.left = '0';
-          this._observer.unobserve(entry.target);
-          this.appendChild(this._image);
-
-          // TODO:
-          // The decoding of large images can block the main thread.
-          // this._image.decode().then(() => {
-          //   this.appendChild(this._image);
-          //   this._image.classList.add('loaded');
-          // }).catch(error => {
-          //   console.error(error);
-          //   throw new Error(`Could not load/decode image (${this._image.src}).`);
-          // });
+          this.enhancePlaceholderImage(entry.target);
         }
       });
     }, {
@@ -34,9 +17,34 @@ class ProgressiveImageElement extends HTMLElement {
   get src() { return this.getAttribute('src'); }
   get srcset() { return this.getAttribute('srcset'); }
 
+  enhancePlaceholderImage() {
+    this._image.onload = e => e.target.classList.add('loaded');
+    this._image.src = this.src;
+    this._image.srcset = this.srcset;
+    this._image.style.position = 'absolute';
+    this._image.style.top = '0';
+    this._image.style.left = '0';
+    this._observer.unobserve(this);
+    this.appendChild(this._image);
+
+    // TODO:
+    // The decoding of large images can block the main thread.
+    // this._image.decode().then(() => {
+    //   this.appendChild(this._image);
+    //   this._image.classList.add('loaded');
+    // }).catch(error => {
+    //   console.error(error);
+    //   throw new Error(`Could not load/decode image (${this._image.src}).`);
+    // });
+  }
+
   connectedCallback() {
+    if (/\slow-2g|2g|3g/.test(navigator.connection.effectiveType)) {
+      this.addEventListener('click', this.enhancePlaceholderImage, { once: true });
+    } else {
+      this._observer.observe(this);
+    }
     this._placeholderImage?.classList.add('loaded');
-    this._observer.observe(this);
   }
 
   disconnectedCallback() {
@@ -45,3 +53,15 @@ class ProgressiveImageElement extends HTMLElement {
 }
 
 export default ProgressiveImageElement;
+
+
+// TODO:
+// function onConnectionChange() {
+//   const { rtt, downlink, effectiveType,  saveData } = navigator.connection;
+//   console.log(`Effective network connection type: ${effectiveType}`);
+//   console.log(`Downlink Speed/bandwidth estimate: ${downlink}Mb/s`);
+//   console.log(`Round-trip time estimate: ${rtt}ms`);
+//   console.log(`Data-saver mode on/requested: ${saveData}`);
+// }
+//
+// navigator.connection.addEventListener('change', onConnectionChange);
