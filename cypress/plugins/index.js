b3ef15f73e1e3d863bee250fd959d93acff51308
André Ruffert <andre@andreruffert.com>
2020-02-06 00:59:10 +0100

    chore(cypress): add proposal-optional-chaining babel plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,12 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const browserify = require('@cypress/browserify-preprocessor');
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  const options = browserify.defaultOptions;
+  const plugins = options.browserifyOptions.transform[1][1].plugins;
+  options.browserifyOptions.transform[1][1].plugins = [
+    ...plugins,
+    '@babel/plugin-proposal-optional-chaining'
+  ];
+
+  on('file:preprocessor', browserify(options));
 }
