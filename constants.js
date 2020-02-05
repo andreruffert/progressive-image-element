2e85a31a57d32b5c2931e7d6019eba3ca0d942a1
André Ruffert <andre@andreruffert.com>
2020-02-05 21:55:08 +0100

    feat: messages for missing placeholder + dimensions

--- constants.js
+++ constants.js
@@ -1,1 +1,9 @@
 export const ELEMENT_NAME = 'progressive-image';
+export const MESSAGES = {
+  placeholderElement: {
+    missing: '<progressive-image> requires a <img> or <svg> element as its only child. See https://andreruffert.github.io/progressive-image-element/examples'
+  },
+  dimensions: {
+    missing: 'An <img> element was loaded, but had no dimensions specified. Specifying dimensions is necessary. See https://github.com/andreruffert/progressive-image-element/pull/4'
+  }
+};
