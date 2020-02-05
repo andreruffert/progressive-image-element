85c418ce97b85f8d63f698edbd472eec48a730ea
André Ruffert <andre@andreruffert.com>
2020-02-05 21:32:50 +0100

    refactor: abstract constants

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import ProgressiveImageElement from './progressive-image-element';
+import { ELEMENT_NAME } from './constants';
 export { ProgressiveImageElement as default }
-
-export const ELEMENT_NAME = 'progressive-image';
 
 if (!window.customElements.get(ELEMENT_NAME)) {
   window.ProgressiveImageElement = ProgressiveImageElement;
